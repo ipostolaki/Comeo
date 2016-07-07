3385fa8e5ec7d44fc41e7d2f288e9a6a44ba8307
Ilia Postolaki <ilia.postolachi@gmail.com>
2016-07-07 13:11:55 +0300

    infrastructure files updates

---
+++ Docker/dev/run_django.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# freeze what was installed during Dockerfile build,
+# file will be saved to the docker-compose mounted volume
+pip freeze > requirements-freezed.txt
+
+# run livereload server as bg job
+python live.py &
+
+# Waiting for pg service is possible with official postgres docker image
+# pg container start to listen to connections, only when setup/start process is complete
+echo "Waiting for the database service..."
+while ! nc -w 1 -z pg_database 5432;
+do
+  echo -n .
+  sleep 1
+done
+
+echo "PostgresSQL database is ready"
+python manage.py runserver 0.0.0.0:80
--- live.py
+++ live.py
@@ -2,6 +2,6 @@ from livereload.server import Server
 
 # Create a new application
 server = Server()
-server.watcher.watch('/Users/ipostolaki/envs/comeo_sync/comeo_project/comeo_app/templates')
-server.watcher.watch('/Users/ipostolaki/envs/comeo_sync/comeo_project/comeo_app/static')
+server.watcher.watch('/source/comeo_project/comeo_app/templates')
+server.watcher.watch('/source/comeo_project/comeo_app/static')
 server.serve(host='127.0.0.1', port='35729', root='.')
