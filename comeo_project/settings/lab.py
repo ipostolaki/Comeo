6362f4a9a356385e80b5d276e0ea73a46881622e
Ilia Postolaki <ilia.postolaki@gmail.com>
2018-02-17 23:18:38 +0100

    Tweaks for current comeo.co domain name

--- comeo_project/settings/lab.py
+++ comeo_project/settings/lab.py
@@ -3,7 +3,7 @@ from .common import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['.comeo.org.md']
+ALLOWED_HOSTS = ['.comeo.co']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_collected')
@@ -12,7 +12,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_collected')
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'contact@comeo.org.md'
+EMAIL_HOST_USER = 'info@comeo.co'
 EMAIL_HOST_PASSWORD = ENV_EMAIL_HOST_PASSWORD
 
 CKEDITOR_UPLOAD_PATH = "ckeditor/"
